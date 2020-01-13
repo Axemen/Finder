50b5af8ce9eb89d0f3cd04f87c84628be784ca2e
Jonathan Randolph <Jrandolph011@gmail.com>
2020-01-12 21:33:07 -0600

    pushed to test-pypi

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows"
+        "Operating System :: OS Independent"
     ],
     python_requires = '>=3.7'
 )
