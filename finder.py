e411c64a2542fcedd8075e90c443993525662b8d
Jonathan Randolph <Jrandolph011@gmail.com>
2020-01-12 20:27:23 -0600

    adding find_files and find_directories functions for general u

--- finder.py
+++ finder.py
@@ -1,10 +1,11 @@
-import glob
-from typing import List, Tuple, Generator
-from pathlib import Path
+from glob import glob, iglob
+from typing import List, Generator
 import os
 """
 A collection of common uses from the glob library wrapped in easy to use functions.
 """
+# @TODO All iglob functions as well.
+# @TODO Rethink function names for easier understanding. 
 
 def find_file_by_extension(root: str, extension: str, recursive = False) -> List:
     """
@@ -12,7 +13,7 @@ def find_file_by_extension(root: str, extension: str, recursive = False) -> List
     extension: the file extension to search for.
     recursive: Weather or not you wish to search sub-directories as well. 
     """
-    return glob.glob(f"{root}**/*{extension}")
+    return glob(f"{root}**/*{extension}")
 
 def find_file_by_name(root:str, file_name:str, recursive = False) -> List:
     """
@@ -20,14 +21,14 @@ def find_file_by_name(root:str, file_name:str, recursive = False) -> List:
     file_name: the name of the file you wish to search for. 
     recursive: Weather or not you wish to search sub-directories as well. 
     """
-    return glob.glob(f"{root}**/{file_name}.*")
+    return glob(f"{root}**/{file_name}.*")
 
 def find_all_files(root:str, recursive = False) -> List:
     """
     root: the root directory to search within.
     recursive: Weather or not you wish to search sub-directories as well. 
     """
-    return glob.glob(f"{root}**/*.*")
+    return glob(f"{root}**/*.*")
 
 def find_directory_by_name(root:str, directory_name: str, recursive = False) -> List:
     """
@@ -35,14 +36,18 @@ def find_directory_by_name(root:str, directory_name: str, recursive = False) -> 
     directory_name: The name of the directory(ies) you wish to find
     recursive: Weather or not you wish to search sub-directories as well. 
     """
-    return glob.glob(f"{root}**/{directory_name}/")
+    return glob(f"{root}**/{directory_name}/")
 
 def find_all_directories(root:str, recursive = False) -> List:
     """
     root: the root directory to search within.
     recursive: Weather or not you wish to search sub-directories as well. 
     """
-    return glob.glob(f"{root}**/")
+    return glob(f"{root}**/")
 
-def find_files_with_folder_in_path(root: str, folder_in_path:str, file_name:str):
-    return glob.glob(f'{root}**/{folder_in_path}/**/{file_name}.*')
+def find_files(root:str, file_name = '**', file_extension = '*', folder_name = '**'):
+    return glob(f"{root}**/{folder_name}/{folder_name}.{file_extension}", recursive = True)
+
+def find_directories(root:str, folder_name = "*", folder_in_path = "**"):
+
+    return glob(f"{root}**/{folder_in_path}/**/{folder_name}/", recursive=True)
